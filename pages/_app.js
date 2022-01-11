df03903c4dd5b8afd2c8d392b50478d86b218c27
afscomercial <afscomercial@gmail.com>
2022-01-10 21:43:14 -0500

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,42 @@
-import '../styles/globals.css'
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
 
-export default MyApp
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../styles/index.scss';
+import Navbar from '../components/shared/Navbar';
+import Hero from '../components/shared/Hero';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:3000/graphql',
+});
+
+const MyApp = ({ Component, pageProps }) => {
+  const isHomePage = () => Component.name === 'Home';
+
+  return (
+    <ApolloProvider client={client}>
+      <div className='portfolio-app'>
+        <Navbar />
+        {isHomePage() && <Hero />}
+        <div className='container'>
+          <Component {...pageProps} />
+        </div>
+        {/* FOOTER STARTS */}
+        {isHomePage() && (
+          <footer
+            id='sticky-footer'
+            className='py-4 bg-black text-white-50 py-3'
+          >
+            <div className='container text-center'>
+              <small>Copyright &copy; Your Website</small>
+            </div>
+          </footer>
+        )}
+        {/* FOOTER ENDS */}
+      </div>
+    </ApolloProvider>
+  );
+};
+
+export default MyApp;
